8af4960690928eeec2c870d563cf1f74a8b7f7f8
mihirKIG <144832738+mihirKIG@users.noreply.github.com>
2025-03-05 20:05:42 +0600

    second_commit

--- remove_book.py
+++ remove_book.py
@@ -1,2 +1,16 @@
+import json
+import os
+from load_book import load_book
+from save_book import save_book
+
 def remove_book():
-    pass
+    books = load_book()
+    isbn = input("Enter ISBN or Book ID to remove: ")
+    
+    for book in books:
+        if book["ISBN"] == isbn:
+            books.remove(book)
+            save_book(books)
+            print("Book removed successfully!")
+            return
+    print("Error: No book found with the given ISBN")
